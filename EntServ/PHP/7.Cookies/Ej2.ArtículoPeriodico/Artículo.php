151be91ea29e1ee442bf94cceb8cbc25eaff2ced
gonlelo <livinkilimanjarogonzalo@gmail.com>
2024-10-09 15:09:37 +0300

    push

--- EntServ/PHP/7.Cookies/Ej2.ArtículoPeriodico/Artículo.php
+++ EntServ/PHP/7.Cookies/Ej2.ArtículoPeriodico/Artículo.php
@@ -4,16 +4,35 @@
     </head>
     <body>
     <?php
-    $lan=es;
-    if ($lan==es) { ?>
+
+    if (isset($_POST["idioma"])){
+        setcookie("idioma", $_POST["idioma"], time() + 3600);
+        $lan = $_POST["idioma"];
+    } elseif (isset($_COOKIE["idioma"])) {
+        $lan = $_COOKIE["idioma"];
+    } else{
+        $lan = "es";
+        setcookie("idioma", "es", time() + 3600);
+    }
+
+    if ($lan=="es") { ?>
+
+        <a href="ElegirIdioma.php">Seleccionar Idioma </a>
         <h1>ALGUIEN HA SALTADO DE UN DECIMOCTAVO PISO Y HA SOBREVIVIDO SIN HACER WATERDROP</h1>
-        <p>Suena como una historia inventada, porque lo es</p>
-    <?php } elseif ($lan==it) {?>
+        <p>Suena como una historia inventada, porque lo es</p>  
+
+    <?php } elseif ($lan=="it") {?>
+
+        <a href="ElegirIdioma.php">Seleccionato lenguayi </a>
         <h1>Prosciutto pizza mario bros</h1>
         <p>mama mia</p>
-   <?php } elseif (lan==prt) { ?>
+
+   <?php } elseif ($lan=="prt") { ?>
+
+    <a href="ElegirIdioma.php">Selecshon de lenguayi </a>
     <h1>Madeiro pasdrinho pepeeeee</h1>
-    <pompom>
+    <p>aaaaaaaaaaaaaaaaaaaa</p>
+
    <?php } ?>
 
     </body>
---
+++ EntServ/PHP/7.Cookies/Ej2.ArtículoPeriodico/ElegirIdioma.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        
+        <form action="Artículo.php" method="post">
+            <select name="idioma">
+                <option value="es">Español</option>
+                 <option value="it">Italiano</option>
+                 <option value="prt">Portugués</option>
+            <input type="submit" value="Calcular" href="Artículo.php" />
+        </form>
+
+    </body>
+</html>
